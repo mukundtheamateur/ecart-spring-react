a45c9e7cb2688bccdd4176664e3ecd25258a2f92
mukundtheamateur <mukundshukla000@gmail.com>
2024-05-25 17:29:45 +0530

    Added few files of backend

---
+++ backend/src/main/java/com/cts/ecart/EcartApplication.java
@@ -0,0 +1,13 @@
+package com.cts.ecart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcartApplication.class, args);
+	}
+
+}
