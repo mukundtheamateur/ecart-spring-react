5ca5af90881076af237054602c6c2508b9261781
mukundtheamateur <mukundshukla000@gmail.com>
2024-05-28 18:30:54 +0530

    added frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import Navbar from './modules/core/components/common/Navbar'
+
+function App() {
+
+  return (
+    <>
+      <Navbar></Navbar>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/modules/core/components/CartCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function CartCard() {
+  return (
+    <div>
+      Cart
+    </div>
+  );
+}
+
+export default CartCard;
